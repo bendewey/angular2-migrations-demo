e2be6078b597799e21863fbbff916f79be52fd8e
Ben Dewey <bdewey01@hotmail.com>
2016-12-07 06:18:48 -0500

    added upgrade-start

---
+++ ngBanking-angular1-upgrade-start/app/accountCmp.ts
@@ -0,0 +1,21 @@
+(function () {
+	'use strict';
+
+	controller.$inject = ['$routeParams', 'GeneralLedger'];
+	function controller($routeParams, generalLedger) {
+		var vm = this;
+
+		vm.account = generalLedger.getById($routeParams.accountId);
+
+		vm.addTransaction = function (transaction) {
+			vm.account.add(transaction);
+		};
+	}
+
+	angular.module('ngBanking').component('accountView', {
+		templateUrl: '/app/account.html',
+		controller: controller,
+		controllerAs: 'vm'
+	});
+
+})();
---
+++ ngBanking-angular1-upgrade-start/app/accountListItemCmp.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular.module('ngBanking').component('accountListItem', {
+        templateUrl: '/app/accountListItem.html',
+        bindings: {
+            account: '<'
+        }
+    });
+})();
+//# sourceMappingURL=accountListItemCmp.js.map
